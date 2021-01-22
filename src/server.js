019ce23fbda8670b5e685d76dfbafedd8d23975d
Richardxugit <cansolion@163.com>
2021-01-22 19:57:47 +1100

    fixed

--- src/server.js
+++ src/server.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
-const app = express();
 
+const app = express();
 const { connectToDB } = require('./utils/db');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
-app.use(cors());
-
 
 connectToDB()
   .then(() => {
